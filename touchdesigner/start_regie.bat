b98637028c03b0fc1cec06c615586ab7d2829559
Aura <projetaura@gmail.com>
2016-02-07 12:18:16 -0500

    Fixed leds controllers

--- touchdesigner/start_regie.bat
+++ touchdesigner/start_regie.bat
@@ -2,7 +2,11 @@ start master/master.toe
 timeout 1
 start master_in/master_in.toe
 timeout 1
-start arbres/arbres.toe
+start arbres_master/arbres_master.toe
+timeout 1
+start arbres_A/arbres_A.toe
+timeout 1
+start arbres_B/arbres_B.toe
 timeout 1
 start lanternes/lanternes.toe
 timeout 1
