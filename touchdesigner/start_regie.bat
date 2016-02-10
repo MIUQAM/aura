afcc626be009c97ed129b1b9dad63c490c29f4df
Projet Aura <projetaura@gmail.com>
2016-02-10 10:27:04 -0500

    Splits master into master + master_dmx

--- touchdesigner/start_regie.bat
+++ touchdesigner/start_regie.bat
@@ -2,6 +2,8 @@ start master/master.toe
 timeout 1
 start master_in/master_in.toe
 timeout 1
+start master_dmx/master_dmx.toe
+timeout 1
 start arbres_master/arbres_master.toe
 timeout 1
 start arbres_A/arbres_A.toe
